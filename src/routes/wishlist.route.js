9d76d6b7f26ccb045dd8c1f5a625c153d1766130
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-08 19:36:06 +0800

    Update code for pageUrl in wishlist.route.js

--- src/routes/wishlist.route.js
+++ src/routes/wishlist.route.js
@@ -6,44 +6,32 @@ const moment = require("moment");
 
 const puppeteer = require("puppeteer");
 
-// router.post("/", async (req, res, next) => {
-//   const wishlistItem = await ctrl.createNewWishlistItem(req.body, next);
-//   console.log(wishlistItem);
-//   res.status(201).json(wishlistItem);
-// });
-// let test = moment().format("DD MMM YYYY, h:mm A").toUpperCase();
-// console.log(test);
-
 router.post("/", async (req, res, next) => {
   let browser = await puppeteer.launch();
   let page = await browser.newPage();
-  let pageUrl = await Object.values(req.body);
-  let finalPageUrl = pageUrl[0];
-  await page.goto(finalPageUrl);
-  // let test = await page.exposeFunction("formatDate", () => {
-  //   moment().format("DD MMM YYYY, h:mm A").toUpperCase();
-  // });
-  // console.log(test);
-  // await page.exposeFunction("moment", moment);
+
+  let pageUrl = req.body.url;
+
+  // -- by using the following:
+  // let pageUrl = await Object.values(req.body);
+  // let finalPageUrl = pageUrl[0];
+  // await page.goto(finalPageUrl);
+  // -- we are allowing the user to pass in as many key/values
+  // -- when they should only be passing in 1 field
+  // -- and it should be specifically "url" key
+
+  await page.goto(pageUrl);
+
   await page.exposeFunction("moment", () =>
     moment().format("DD MMM YYYY, h:mm A")
   );
 
   let itemDetails = await page.evaluate(async () => {
-    // let ts = Date.now();
-    // let date_ob = new Date(ts);
-    // let date = date_ob.getDate();
-    // let month = date_ob.getMonth() + 1;
-    // let year = date_ob.getFullYear();
-
     let productLink = window.location.href;
     let productName = document.querySelector(".product-name").innerText;
     let originalPrice = document.querySelector(".price-standard").innerText;
     let salesPrice = document.querySelector(".price-sales").innerText.trim();
     let lastUpdated = await window.moment();
-    // let lastUpdated = await window.moment(timestamp).format("DD MMM YYYY");
-    // let lastUpdated = moment().format("DD MMM YYYY, h:mm A").toUpperCase();
-    // let lastUpdated = Date.now();
 
     return {
       productLink,
