dff3a476885be8e6c61be6f8a21b113676d16cc0
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-08 10:47:35 +0800

    Update app.js and index.js file

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const win = {
+  0: "GET    /",
+  1: "GET    /dumplings",
+  2: "POST   /dumplings",
+  3: "GET    /dumplings/:name",
+  4: "PUT    /dumplings/:id",
+  5: "DELETE /dumplings/:id",
+  6: "-----------------------",
+  7: "GET    /dumplings/presenter",
+};
+app.get("/", (req, res) => {
+  res.status(200).json(win);
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+const PORT = 3009;
+
+const server = app.listen(PORT, () => {
+  console.log(`Express app started on http://localhost:${PORT}`);
+});
