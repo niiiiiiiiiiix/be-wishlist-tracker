066f57eb46067a289015fdef2b335721cb57f8de
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-10 10:40:11 +0800

    Remove dumplings base case

--- src/app.js
+++ src/app.js
@@ -7,20 +7,6 @@ app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-const win = {
-  0: "GET    /",
-  1: "GET    /dumplings",
-  2: "POST   /dumplings",
-  3: "GET    /dumplings/:name",
-  4: "PUT    /dumplings/:id",
-  5: "DELETE /dumplings/:id",
-  6: "-----------------------",
-  7: "GET    /dumplings/presenter",
-};
-app.get("/", (req, res) => {
-  res.status(200).json(win);
-});
-
 const userRouter = require("./routes/user.route");
 app.use("/", userRouter);
 
