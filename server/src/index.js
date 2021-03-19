4deba28e48fbfac8f0326e995d9b5a5075fcae81
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-19 12:20:52 +0800

    Change PORT variable in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 require("./utils/db");
 
 const app = require("./app");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Express app started on http://localhost:${PORT}`);
