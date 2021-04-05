8132a14cea7e577e278e954f02a2efc9ca0c0cbd
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-04-05 14:08:00 +0800

    Add test for POST /user/signup for brand new user

--- server/__tests__/user.test.js
+++ server/__tests__/user.test.js
@@ -3,6 +3,7 @@ const app = require("../src/app");
 // const User = require("../src/models/user.model");
 const dbHandlers = require("../test/dbHandler");
 const createJWTToken = require("../src/config/jwt");
+const bcrypt = require("bcryptjs");
 
 describe("User", () => {
   beforeAll(async () => {
@@ -20,8 +21,21 @@ describe("User", () => {
   describe("GET /user", () => {
     it("should return welcome message", async () => {
       const { text } = await request(app).get("/user").expect(200);
-
       expect(text).toEqual("Welcome!");
+    });
+  });
+
+  describe("POST /user/signup", () => {
+    const user = { username: "admin001", password: "admin001" };
+    it("should create new user", async () => {
+      const response = await request(app).post("/user/signup").send(user);
+      console.log(response);
+      expect(response.status).toEqual(201);
+      expect(response.body.username).toEqual(user.username);
+      expect(
+        await bcrypt.compare(user.password, response.body.password)
+      ).toEqual(true);
+      expect(response.body.wishlist).toEqual([]);
     });
   });
 });
